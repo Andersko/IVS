30161b3e8fc3d94588af06c6b0559bcfb462b1d7
Adam Kostolányi <akostolanyi@gmail.com>
2022-04-12 22:29:00 +0200

    Fixed bug with negative numbers

--- src/expression_parser.py
+++ src/expression_parser.py
@@ -111,7 +111,7 @@ def calculate_expression( operation, numberList):
         return False
 
 def parse_string(text):
-        regex = re.compile(r'((?:\-?(?:\d+(?:\.\d+)?))|(?:[\-\+\/\*\(\)\√\^\%]))')
+        regex = re.compile(r'((?:(?<!\d)\-?(?:\d+(?:\.\d+)?))|(?:[\-\+\/\*\(\)\√\^\%]))')
         parsed = re.findall(regex,text)
         print(parsed)
         parsed = [ float(x) if (re.match( r'(-?(?:\d*\.\d+|\d+))' ,x) != None) else x for x in parsed]
